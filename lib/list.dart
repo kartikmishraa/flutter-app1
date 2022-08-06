681356b0897036f3747eb89ea921857b6ca10746
Kartik Mishra <kartikm2219@gmail.com>
2022-08-06 21:45:32 +0530

    temp

--- lib/list.dart
+++ lib/list.dart
@@ -7,14 +7,13 @@ const String logoPath = "assets/images/mikey.jpg";
 const String heroTag = "logo";
 
 class AnimeList extends StatefulWidget {
-  const AnimeList({ Key? key }) : super(key: key);
+  const AnimeList({Key? key}) : super(key: key);
 
   @override
   _AnimeListState createState() => _AnimeListState();
 }
 
 class _AnimeListState extends State<AnimeList> {
-  
   static const String bgImgPath = "assets/images/wallpaper1.jpg";
   static const List<String> animeTitles = [
     "Demon Slayer",
@@ -31,7 +30,7 @@ class _AnimeListState extends State<AnimeList> {
     'https://wallpaperaccess.com/horimiya',
     'https://wallpaperaccess.com/black-clover-mobile',
   ];
-  
+
   SizedBox mySizedBox(double height_, double width_) {
     return SizedBox(
       height: height_,
@@ -39,36 +38,38 @@ class _AnimeListState extends State<AnimeList> {
     );
   }
 
-  ElevatedButton myElevatedButton(int animeNum, double paddingH, double paddingV) {
+  ElevatedButton myElevatedButton(
+      int animeNum, double paddingH, double paddingV) {
     return ElevatedButton(
       onPressed: () async {
-        if (!await launch(url[animeNum], forceWebView: false,  enableJavaScript: true)) {
-        throw "Could not launch";
+        if (!await launch(url[animeNum],
+            forceWebView: false, enableJavaScript: true)) {
+          throw "Could not launch";
         }
-      },  
+      },
       child: Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: paddingH,
-                vertical: paddingV,
-              ),
-              child: Text(
-                animeTitles[animeNum],
-                style: const TextStyle(
-                  fontFamily: baseFont,
-                  fontSize: 30,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-         style: ButtonStyle(
-           backgroundColor: MaterialStateProperty.resolveWith<Color>(
-               (Set<MaterialState> states) {
-                 if (states.contains(MaterialState.pressed)) return Colors.yellow;
-                 return Colors.amber.shade600;
-               },
-         ),
+        padding: EdgeInsets.symmetric(
+          horizontal: paddingH,
+          vertical: paddingV,
+        ),
+        child: Text(
+          animeTitles[animeNum],
+          style: const TextStyle(
+            fontFamily: baseFont,
+            fontSize: 30,
+            color: Colors.black,
+          ),
+        ),
       ),
-      );
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+          (Set<MaterialState> states) {
+            if (states.contains(MaterialState.pressed)) return Colors.yellow;
+            return Colors.amber.shade600;
+          },
+        ),
+      ),
+    );
   }
 
   @override
@@ -76,11 +77,8 @@ class _AnimeListState extends State<AnimeList> {
     return SafeArea(
       child: Container(
         decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(bgImgPath)
-          ),
+          image: DecorationImage(image: AssetImage(bgImgPath)),
         ),
-
         child: Scaffold(
           appBar: AppBar(
             actions: [
@@ -101,47 +99,36 @@ class _AnimeListState extends State<AnimeList> {
                 color: Colors.black,
                 fontFamily: baseFont,
                 fontSize: 35,
-                ),
               ),
+            ),
             backgroundColor: const Color(0xffeba10e),
             leading: const Hero(
-              tag: heroTag, 
+              tag: heroTag,
               child: CircleAvatar(
                 backgroundImage: AssetImage(logoPath),
               ),
             ),
           ),
-          
           backgroundColor: Colors.transparent,
-          
           body: SingleChildScrollView(
-            child: Center(child: Column(
-              children: [
-                mySizedBox(40, 0),
-
-                myElevatedButton(0, 35, 10),
-
-                mySizedBox(40, 0),
-
-                myElevatedButton(1, 75, 10),
-
-                mySizedBox(40, 0),
-
-                myElevatedButton(2, 25, 10),
-
-                mySizedBox(40, 0),
-
-                myElevatedButton(3, 65, 10),
-
-                mySizedBox(40, 0),
-
-                myElevatedButton(4, 45, 10),
-
-                mySizedBox(40, 0),
-              ],
+            child: Center(
+              child: Column(
+                children: [
+                  mySizedBox(40, 0),
+                  myElevatedButton(0, 35, 10),
+                  mySizedBox(40, 0),
+                  myElevatedButton(1, 75, 10),
+                  mySizedBox(40, 0),
+                  myElevatedButton(2, 25, 10),
+                  mySizedBox(40, 0),
+                  myElevatedButton(3, 65, 10),
+                  mySizedBox(40, 0),
+                  myElevatedButton(4, 45, 10),
+                  mySizedBox(40, 0),
+                ],
+              ),
+            ),
           ),
-          ),
-        ),
         ),
       ),
     );
